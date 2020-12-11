3d3f01be406c01988560b64393d61934db42a3be
koishito <64292509+koishito@users.noreply.github.com>
2020-12-12 01:25:29 +0900

    bakground.jsにて、DOM要素を操作するには？

--- js/background.js
+++ js/background.js
@@ -13,25 +13,7 @@ chrome.runtime.onInstalled.addListener(function (details) {
     }
   });
 
-  document.addEventListener('scroll',function() {
-    const scrollHeight = Math.max(
-      document.body.scrollHeight, document.documentElement.scrollHeight,
-      document.body.offsetHeight, document.documentElement.offsetHeight,
-      document.body.clientHeight, document.documentElement.clientHeight
-    );
-    var scrollTop =
-    document.documentElement.scrollTop || // IE、Firefox、Opera
-    document.body.scrollTop;              // Chrome、Safari
-  
-    console.log("scrollHeight : " + scrollHeight);
-    console.log("window.innerHeight : " + window.innerHeight);
-    console.log("bottom : " + (scrollHeight - window.innerHeight));
-    console.log("currrent : " + document.documentElement.scrollTop);
-    console.log("Difference : " + parseInt(scrollHeight - window.innerHeight - document.documentElement.scrollTop));
-    if(parseInt(scrollHeight - window.innerHeight - document.documentElement.scrollTop) == 0) {
-      alert("bottom");
-    };
-  }, {passive: true});
+
   
 });
 
