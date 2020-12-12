8be23b24d62255e098e71bbe74be2a4817d5da3f
koishito <64292509+koishito@users.noreply.github.com>
2020-12-12 11:21:16 +0900

    Organize

--- js/contents.js
+++ js/contents.js
@@ -1,10 +1,22 @@
-// 送信側 contents -> background
-chrome.runtime.sendMessage(
-    { value: { contents: "test value from contents" } }
-);
+// // 送信側 contents -> background
+// chrome.runtime.sendMessage(
+//     { value: { contents: "test value from contents" } }
+// );
 
-// 受信側 other tab -> contents(popup/option -> contents)
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-    console.log(message);
-    return;
-});
+// // 受信側 other tab -> contents(popup/option -> contents)
+// chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+//     console.log(message);
+//     return;
+// });
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+      if( request.message === "start" ) {
+       start();
+           }
+    }
+  );
+
+  function start(){
+      alert("started");
+  }
--- js/popup.js
+++ js/popup.js
@@ -1,19 +1,30 @@
-// backgroundで受け取った値をコンソールに表示
-function logBackgroundValue () {
-    var test = chrome.extension.getBackgroundPage().test_value;
-    console.log(test);
-    return;
+// // backgroundで受け取った値をコンソールに表示
+// function logBackgroundValue () {
+//     var test = chrome.extension.getBackgroundPage().test_value;
+//     console.log(test);
+//     return;
+// }
+
+// // 現在アクティブなタブにデータを送信
+// function sendToContents(){
+//     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+//         chrome.tabs.sendMessage(tabs[0].id, 
+//             JSON.stringify({ contents: "test value from popup" }),
+//             function (response) {
+//             });
+//     });    
+// }
+
+// document.getElementById('log').addEventListener('click', logBackgroundValue);
+// document.getElementById('send').addEventListener('click', sendToContents);
+
+function popup() {
+    chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
+    var activeTab = tabs[0];
+    chrome.tabs.sendMessage(activeTab.id, {"message": "start"});
+   });
 }
 
-// 現在アクティブなタブにデータを送信
-function sendToContents(){
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, 
-            JSON.stringify({ contents: "test value from popup" }),
-            function (response) {
-            });
-    });    
-}
-
-document.getElementById('log').addEventListener('click', logBackgroundValue);
-document.getElementById('send').addEventListener('click', sendToContents);
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("button1").addEventListener("click", popup);
+});
